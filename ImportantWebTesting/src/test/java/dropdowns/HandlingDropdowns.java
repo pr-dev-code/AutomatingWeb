71c29d0afcdd5bd40658141550be111e6acd35d4
Pradumna Pratap Singh <singhpradumnapatap@gmail.com>
2025-09-25 00:00:07 +0530

    Addition

--- ImportantWebTesting/src/test/java/dropdowns/HandlingDropdowns.java
+++ ImportantWebTesting/src/test/java/dropdowns/HandlingDropdowns.java
@@ -13,6 +13,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class HandlingDropdowns {
 
 	public static void main(String[] args) {
+		// This is Dropdown Testing
+		 
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.wikipedia.org/");
